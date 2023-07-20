6ec40b96349e1216500656c8273661f648d271ce
kunal nayak <87607403+Kunal766@users.noreply.github.com>
2023-07-20 20:06:01 +0530

    Initial Commit

---
+++ Tokenizer/hardhat.config.ts
@@ -0,0 +1,29 @@
+import { HardhatUserConfig, task } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-chai-matchers";
+import "@nomiclabs/hardhat-ethers";
+require('dotenv').config()
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.17",
+
+  networks: {
+    avalanche_fuji: {
+      url: "https://avalanche-fuji-c-chain.publicnode.com",
+
+      accounts: {
+        mnemonic: process.env.RECOVERY_PHRASE,
+      },
+    },
+  },
+};
+
+export default config;
